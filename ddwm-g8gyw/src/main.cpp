b9c6860e595214d3581c9132027d6705f6c09551
owen <owen@owenduffy.net>
2022-10-27 04:36:20 +1100

    Major rework, tested.

--- ddwm-g8gyw/src/main.cpp
+++ ddwm-g8gyw/src/main.cpp
@@ -1,6 +1,7 @@
 // https://g8gyw.github.io/
 //
 // Copyright (c) 2021 Mike G8GYW
+// Revision 2022 Owen Duffy
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -12,9 +13,6 @@
 // The above copyright notice and this permission notice shall be included in all
 // copies or substantial portions of the Software.
 
-// Use of the MegunoLINK filter library is licensed by the copyright owners under the terms of the
-// GNU Lesser General Public License: https://github.com/Megunolink/MLP/blob/master/LICENSE.
-
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -23,150 +21,199 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-// This program is designed to run on an Atmega328P with internal 8MHz clock and internal bandgap voltage reference.
-// It takes the outputs from a Stockton bridge using a 10:1 transformer turns ratio and 1N5711 Scottky diodes,
-// applies calibration factors then calculates and displays the average forward power, VSWR and battery voltage.
+// This program is designed to run on an Atmega328P with 8MHz clock.
 
 // ATMEGA FUSE VALUES
-// E:FF   H:D7   L:E2
+// L:FF   H:D3   E:FD //NOTE: this specifies an external crystal as used in a Chinese copy
+// L:E2   H:D3   E:FD //NOTE: this specifies the internal RC oscillator as used in G8GYW's article
+
+#define VERSION "0.02"
+#define READ_INTEEPROM
+//#define WRITE_INTEEPROM
+//#define CALMODE
 
 #include <Wire.h>
 #include <Adafruit_SSD1306.h>
-#include <Filter.h>
-
-#define VERSION   "v1.06"
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
 #define OLED_RESET    -1 // Reset pin # (or -1 if sharing Arduino reset pin)
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+int barh=SCREEN_HEIGHT/2;
 
-// DEFINE VARIABLES
+#if defined(__AVR_ATmega328P__)
+HardwareSerial &MySerial=Serial;
+#include <EEPROM.h>
+#endif
+
+struct{
+  uint16_t ever=0;
+  #if defined(CALMODE)
+  uint16_t avgn=4*2;
+  #else
+  uint16_t avgn=2;
+  #endif
+  float pmin=0.05;
+  int adcoffsadj=0;
+  uint16_t flags=0;
+  float vreff=1.0718*(18+10)/10; //MeasuredAdcRef*(R3+R4)/R4
+  float vrefr=1.1225/1.1173*1.0718*(18+10)/10; //MeasuredAdcRef*(R7+R8)/R8 //adjusted to calibrate to vreff
+  float a=0.039358125429379,b=0.92716713641334,c=0.91574481497507,d=0.11403999912754;
+} parms;
+int adcpf=A0,adcpr=A1; // select the input pin for the detector
+unsigned AdcAccumulator; // variable to accumulate the value coming from the sensor
+float calfactorf,calfactorr;
 
 float Vfwd;   // Forward voltage
 float Vrev;   // Reverse voltage
 float Pfwd;   // Forward power
 float Prev;   // Reverse power
 float SWR;    // VSWR
-float Gamma;  // Reflection coefficient
+float rho;  // Reflection coefficient
 float Vbat;   // Battery voltage
 
-// CALIBRATION FACTORS
-// The tolerance on the Atmega328P's internal voltage reference is 1.0 to 1.2 Volts.
-// The actual voltage should be measured on pin 21 and entered below.
+void setup(){
+  float adcref;
+  long adcfs;
+  int ever;
 
-// The response of the 1N5711 diodes follows a curve represented by the equation:
-// Pfwd = aVfwd^2 + bVfwd (and the same for Prev and Vrev)
-// where a and b are constants determined by plotting Power In vs Vfwd and performing a curve fit.
-// An excellent tool for this can be found at https://veusz.github.io/
-// The values of a and b below can be adjusted if necessary to improve accuracy (try adjusting b first).
-
-const float IntRef = 1.1;
-const float a = 1.0;
-const float b = 0.75;
-
-// FUNCTIONS TO CALCULATE FORWARD AND REVERSE POWER
-
-float CalculatePfwd ()
-{
-  float Vadc0 = analogRead(A0); // Read ADC0 (pin 23)
-  Vadc0 = constrain(Vadc0, 1, 1023); // Prevent divide-by-zero when calculating Gamma
-  Vfwd = 2.8 * ((Vadc0 + 0.5) * IntRef / 1024); // Scaled up by R3 & R4
-  Pfwd = a * sq(Vfwd) + b * Vfwd;
-  return Pfwd;
-}
-
-float CalculateSWR ()
-{
-  float Vadc1 = analogRead(A1); // Read ADC1 (pin 24)
-  Vrev = 2.8 * ((Vadc1 + 0.5) * IntRef / 1024); // Scaled up by R7 & R8
-  Prev = a * sq(Vrev) + b * Vrev;
-  if (Prev < 0.01)
-  { //exclude residual values
-    Prev = 0;
-  }
-  Gamma = sqrt(Prev / Pfwd); // Calculate reflection coefficient
-
-  SWR = (1 + Gamma) / (1 - Gamma);
-  SWR = constrain(SWR, 1, 99.9);
-  return SWR;
-}
-
-// ADC FILTER
-// A recursive filter removes jitter from the readings using the MegunoLINK filter library:
-// https://www.megunolink.com/documentation/arduino-libraries/exponential-filter/
-
-// Create new exponential filters with a weight of 90 and initial value of 0
-// Adjust these values as required for a stable display
-
-ExponentialFilter<float> FilteredPfwd(50, 0);
-ExponentialFilter<float> FilteredVSWR(50, 0);
-
-void setup()
-{
-  ADCSRA &= ~(bit (ADPS0) | bit (ADPS1) | bit (ADPS2)); // Clear prescaler bits
-  ADCSRA |= bit (ADPS1) | bit (ADPS2);                  // Set prescaler to 64
-
-// Enable the internal voltage reference and disable unused digital buffers
-  analogReference (INTERNAL);
-  bitSet (DIDR0, ADC3D);
-  bitSet (DIDR0, ADC4D);
-  bitSet (DIDR0, ADC5D);
-
-  analogRead (A2);  // Read ADC2
+  #if defined(__AVR_ATmega328P__)
+  pinMode(A0,INPUT);
+  analogReference(INTERNAL);
+  adcref=1.10;
+  adcfs=1024;
+  #endif
+  analogRead(A2); //Read ADC2
   delay (500); // Allow ADC to settle
-  float Vpot = analogRead (A2); // Read ADC again
-  Vbat = 4.9 * ((Vpot + 0.5) * IntRef / 1024); // Calculate battery voltage scaled by R9 & R10
-
-// Display startup screen  
-  Serial.begin(9600);
+  float vbat=analogRead(A2); //Read ADC again
+  vbat=4.9*(vbat + 0.5)/(float)adcfs*adcref; //Calculate battery voltage scaled by R9 & R10
+  // Display startup screen
+  MySerial.begin(9600);
+  MySerial.println(F("Starting..."));
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  //Initialize with the I2C address 0x3C.
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(0, 0);
-  display.print("   POWER/SWR METER   ");
+  display.print("Directional wattmeter");
   display.setCursor(0, 16);
-  display.print("    12 Watts maximum    ");
-  display.setCursor(0, 32);
-  display.print("Battery volts = ");
-  display.print(Vbat);
-  display.setCursor(0, 48);
-  display.print("--------");
-  display.print(VERSION);
-  display.print("--------");
+  display.setTextSize(2);
+  display.print(" 12W MAX");
+  display.setTextSize(1);
   display.display();
-  delay(5000);        // delay 5 seconds
+  delay(1000);
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.print("ddwm ver: ");
+  display.println(VERSION);
+  display.print("vbat: ");
+  display.println(vbat,1);
+  display.display();
+  delay(1000);
 
+  #if defined(WRITE_INTEEPROM)
+  MySerial.println(F("writing eeprom..."));
+  EEPROM.put(0x0,parms);
+  MySerial.println(F("done"));
+  while(1);
+  #endif
+  #if defined(READ_INTEEPROM)
+  //get the data block from eeprom
+  ever=EEPROM.read(0);
+  if(ever!=0){
+    MySerial.print(F("Unsupported EEPROM version: "));
+    MySerial.println(ever);
+    while(1);
+  }
+  EEPROM.get(0,parms);
+  MySerial.println(F("Read EEPROM."));
+  #endif
+
+  calfactorf=parms.vreff/parms.avgn/(float)adcfs;
+  calfactorr=parms.vrefr/parms.avgn/(float)adcfs;
 }
 
-void loop()
-{
-  float Power = CalculatePfwd(); // Get new value of forward power
-  FilteredPfwd.Filter(Power); // Apply filter to new value
-  float SmoothPower = FilteredPfwd.Current(); // Return current value of filter output
+void loop(){
+  int prec;
+  int i;
+  float vinf,pwrf,vinr,pwrr,dbm;
+  long AdcAccumulatorf=0,AdcAccumulatorr=0;
 
-  float VSWR = CalculateSWR(); // Get new value of VSWR
-  FilteredVSWR.Filter(VSWR); // Apply filter to new value
-  float SmoothVSWR = FilteredVSWR.Current(); // Return current value of filter output
-
-// Display Power and VSWR  
+  for(i=parms.avgn;i--;){
+    // read the value from the detector
+    AdcAccumulatorf+=analogRead(adcpf);
+    AdcAccumulatorr+=analogRead(adcpr);
+    delay(100);
+    }
+  // calculate average v
+  vinf=((float)AdcAccumulatorf+(float)parms.adcoffsadj*(float)parms.avgn)*calfactorf;
+  vinr=((float)AdcAccumulatorr+(float)parms.adcoffsadj*(float)parms.avgn)*calfactorr;
+  #if defined(CALMODE)
+  pwrf=vinf;
+  pwrr=vinr;
+  #else
+  pwrf=parms.a+parms.b*vinf+parms.c*pow(vinf,2)+parms.d*pow(vinf,3);
+  pwrr=parms.a+parms.b*vinr+parms.c*pow(vinr,2)+parms.d*pow(vinr,3);
+  #endif
+/*
+  MySerial.print(F("AdcAccumulatorf: "));
+  MySerial.print(AdcAccumulatorf);
+  MySerial.print(F(", vinf: "));
+  MySerial.print(vinf,10);
+  MySerial.print(F(", pwrf: "));
+  MySerial.println(pwrf,10);
+  MySerial.print(F("AdcAccumulatorr: "));
+  MySerial.print(AdcAccumulatorr);
+  MySerial.print(F(", vinr: "));
+  MySerial.print(vinr,10);
+  MySerial.print(F(", pwrr: "));
+  MySerial.println(pwrr,10);
+*/
+  //display Power and VSWR
   display.clearDisplay();
   display.setTextSize(2);
-  display.setCursor (12, 12);
-  display.print("PWR: ");
-  display.setCursor (60, 12);
-  display.print(SmoothPower, 1); // Display forward power to one decimal place
-  display.print("W");
-  display.setCursor (12, 36);
-  display.print("SWR: ");
-  display.setCursor (60, 36);
-  if (Pfwd < 0.01)
-  { display.print("**.*"); // Blank invalid readings
-  }
-   else {
-  display.print(SmoothVSWR, 1); // Display VSWR to one decimal place
-  }
-  display.display();
+  display.setCursor (0,0);
+  #if defined(CALMODE)
+  display.println(pwrf,8);
+  display.println(pwrr,8);
+  #else
+  if(pwrf<parms.pmin){
+    pwrf=0.0;
+    dbm=-99;
+    prec=3;
+    }
+  else{
+    dbm=10*log10(pwrf/0.001);
+    prec=4-floor(dbm/10);
+    }
+  if(pwrf>=parms.pmin){
+    prec+=1;
+    display.print(pwrf,prec);
+    display.print(F("W"));
+    }
+  else
+    display.print(F("***W"));
 
-}
+  if(pwrf>parms.pmin && pwrr>parms.pmin){
+    rho=sqrt(pwrr/pwrf); // Calculate reflection coefficient
+  //  MySerial.print(F(", rho: "));
+  //  MySerial.println(rho,10);
+    rho=constrain(rho,0,0.99999);
+    SWR=(1+rho)/(1-rho);
+    SWR=constrain(SWR,1,99.9);
+//    MySerial.print(F(", swr: "));
+//    MySerial.println(SWR,10);
+    int w=((SWR-1)*32)+0.5+65;
+    // Draw filled part of bar starting from left of screen:
+    display.fillRect(65,display.height()-1-barh,w,barh,1);
+    display.fillRect(w+1,display.height()-barh-1,display.width()-w,barh,0);
+    for(int i=80;i<128;i=i+16)
+      display.fillRect(i,display.height()-1-barh/4,1,barh/4,0);
+    for(int i=96;i<128;i=i+32)
+      display.fillRect(i,display.height()-1-barh/2,1,barh/2,0);
+    display.setCursor(0,17);
+    display.print(SWR,1); // Display VSWR to one decimal place
+    }
+#endif
+  display.display();
+  }
